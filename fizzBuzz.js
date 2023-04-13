2715e430c3729429f526ced81abf224c6c1368ee
Garrett Pace <elderpace6@gmail.com>
2023-04-13 10:40:12 -0600

    pat self on back

---
+++ fizzBuzz.js
@@ -0,0 +1,66 @@
+// Write your solution below this line:
+
+// Write a program that counts from 1 to 50 in fizzbuzz fashion.
+
+// To do so, loop from 1 to 50 (inclusive). Each time through, if the number is evenly divisible by 3, say ‘fizz’.
+// If the number is evenly divisible by 5, say ‘buzz’. If the number is evenly divisible by both 3 and 5, say ‘fizzbuzz’.
+// Otherwise, say the number.
+
+
+
+
+if (age = 17)
+
+// DESCRIPTION:
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+// Examples: (Input --> Output)
+
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
+
+if(age < 14){
+    console.log("drink toddy")
+} else if (age < 18){
+    console.log( "drink coke")
+}else if (age <21){
+    console.log("drink beer")
+}else {
+    console.log("drink whisky")
+}
+
+age = 30
+
+if(age < 14){
+    console.log("drink toddy")
+} else if (age < 18){
+    console.log( "drink coke")
+}else if (age <21){
+    console.log("drink beer")
+}else {
+    console.log("drink whisky")
+}
+
+age = 30
+
+age < 14){
+    console.log("drink toddy")
+} else if (age < 18){
+    console.log( "drink coke")
+}else if (age <21){
+    console.log("drink beer")
+}else {
+    console.log("drink whisky")
+}
